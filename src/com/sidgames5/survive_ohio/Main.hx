3bb017a98765055336d593f36d29a3ec932c9efe
Sid <saytleader@outlook.com>
2023-03-06 13:05:21 -0500

    Update Main.hx

--- src/com/sidgames5/survive_ohio/Main.hx
+++ src/com/sidgames5/survive_ohio/Main.hx
@@ -1,15 +1,9 @@
 package com.sidgames5.survive_ohio;
 
-import hxd.App;
-import hxd.Text
-import hxd.res.DefaultFont;
+import openfl.display.Sprite;
 
-class Main extends App {
-    override function init() {
-        var tf = new Text(DefaultFont.get(), s2d);
-        tf.text = "Hello World !";
-    }
-    static function main() {
-        new Main();
-    }
+class Main extends Sprite {
+	public static function main() {
+		
+	}
 }
