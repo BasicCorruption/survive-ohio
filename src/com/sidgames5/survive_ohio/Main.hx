82be6a5581da693638ab38b0c6683662e1b27954
Sid <saytleader@outlook.com>
2023-03-07 12:16:12 -0500

    Update Main.hx

--- src/com/sidgames5/survive_ohio/Main.hx
+++ src/com/sidgames5/survive_ohio/Main.hx
@@ -1,9 +1,10 @@
 package com.sidgames5.survive_ohio;
 
 import openfl.display.Sprite;
+import openfl.Lib;
 
 class Main extends Sprite {
 	public static function main() {
-		
+		Loader.preInitialize();
 	}
 }
