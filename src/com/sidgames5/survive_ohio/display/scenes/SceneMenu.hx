63f0ad8bede983a8f608cac7a28adbf3e7729d2c
Sid <saytleader@outlook.com>
2023-03-07 12:42:35 -0500

    Create SceneMenu.hx

---
+++ src/com/sidgames5/survive_ohio/display/scenes/SceneMenu.hx
@@ -0,0 +1,10 @@
+package com.sidgames5.survive_ohio.display.scenes;
+
+class SceneMenu extends Scene {
+	override public function create() {
+		
+	}
+	override public function update(dt:Float) {
+		super.update(dt);
+	}
+}
